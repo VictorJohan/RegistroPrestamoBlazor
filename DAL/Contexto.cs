41170ef948e6fb689cb14625f6914c7cccebf0a3
Victor Johan <Vicjohan23@gmail.com>
2021-01-21 16:33:14 -0400

    SE CREO EL CONTEXTO Y SE HIZO LA CONECCION A LA BASE DE DATOS

---
+++ DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroPrestamoBlazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPrestamoBlazor.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+        public DbSet<Prestamos> Prestamos { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
