159a2be5a9c324cd41f8de1453db47dd0f1aa9e8
Victor Johan <Vicjohan23@gmail.com>
2021-01-22 01:36:20 -0400

    SE AGREGARON RESTRICCIONES EN LA ENTIDAD Prestamos

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -12,9 +12,12 @@ namespace RegistroPrestamoBlazor.Models
         [Key]
         public int PrestamoId { get; set; }
         public DateTime Fecha { get; set; } = DateTime.Now;
+        [Required(ErrorMessage ="Es obligatorio introducir un concepto.")]
         public string Concepto { get; set; }
+        [Range(minimum:100, maximum:10000)]
         public double Monto { get; set; }
         public double Balance { get; set; }
+        [Required(ErrorMessage ="Debe seleccionar un persona para asignar el prestamo.")]
         public int PersonaId { get; set; }
 
         [ForeignKey("PersonaId")]
